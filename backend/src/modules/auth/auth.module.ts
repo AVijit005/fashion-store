853078a92df3b312c32d17d26a4b1197dd444cc5
Avijit Pal <palavijit713130@gmail.com>
2026-05-30 16:50:55 +0530

    Fix NestJS dependency injection crash for ThrottlerGuard on Render

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [UsersModule, JwtModule.register({})],
+  imports: [UsersModule, JwtModule.register({}), ThrottlerModule],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService, JwtModule],
